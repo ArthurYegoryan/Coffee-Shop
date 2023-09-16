c372354c547cbf9a414b78fd3c1cffc04ee9e18c
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-09-16 16:05:28 +0400

    feature(Tatyana): change structure and add addOrders method

--- coffeeShop.js
+++ coffeeShop.js
@@ -1,29 +1,41 @@
 class CoffeeShop {
-    constructor(name) {
+    constructor(name, menu) {
         this.name = name;
+        this.orders = [];
+        this.menu = manu;
     }
 
-    menu = [
-        {
-            food: {
-                pizza: 1000,
-                hotDog: 400,
-                burger: 1500,
-            },
-        },
-        {
-            drink: {
-                cappuccino: 1000,
-                amerikano: 800,
-                esspresso: 700,
-                glasse: 1200,
-            },
-        }
-    ];
-
-    orders = [];
-
+    
     addOrder(item) {
-        
+        if(this.menu[item.name]) {
+            this.orders.push(item.name)
+        } else {
+            return `This item is currently unavailable! ${this.name}s coffee shop does not sell ${item.name}`
+        }
     }
+
 }
+const menu = [
+    {
+      name: 'coca_cola',
+      type: 'drink',
+      price: 20,
+    },
+    {
+      name: 'pizza',
+      type: 'food',
+      price: 30,
+    },
+    {
+      name: 'cheese',
+      type: 'food',
+      price: 18,
+    },
+    {
+      name: 'coffee',
+      type: 'drink',
+      price: 25,
+    },
+  ];
+
+const Jazzve = new CoffeeShop('Jazzve', menu)
