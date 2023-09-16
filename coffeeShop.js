7e040227b5cf841e3f320f673d6d8493e63dc670
Arthur Yegoryan <arthur.yegoryan.97@gmail.com>
2023-09-16 15:48:02 +0400

    [200~

---
+++ coffeeShop.js
@@ -0,0 +1,29 @@
+class CoffeeShop {
+    constructor(name) {
+        this.name = name;
+    }
+
+    menu = [
+        {
+            food: {
+                pizza: 1000,
+                hotDog: 400,
+                burger: 1500,
+            },
+        },
+        {
+            drink: {
+                cappuccino: 1000,
+                amerikano: 800,
+                esspresso: 700,
+                glasse: 1200,
+            },
+        }
+    ];
+
+    orders = [];
+
+    addOrder(item) {
+        
+    }
+}
